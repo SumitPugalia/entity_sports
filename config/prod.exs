cf077a36e6b6fafcfbc405763d806559f29d2c41
Sumit Pugalia <sumit_pugalia@yahoo.com>
2024-05-11 00:00:28 +0530

    remove cred

--- config/prod.exs
+++
@@ -1,7 +0,0 @@
-import Config
-
-config :entity_sports,
-  http_client_opts: [],
-  client_module: EntitySports.Client,
-  base_url: "https://rest.entitysport.com/v2/",
-  token: "ec471071441bb2ac538a0ff901abd249"
--- config/test.exs
+++ config/test.exs
@@ -2,8 +2,6 @@ import Config
 
 config :entity_sports,
   http_client_opts: [],
-  merchant_id: "fake",
-  client_id: "fake",
-  secret_key: "fake",
-  bill_desk_endpoint: "",
-  client_module: EntitySports.Stub
+  client_module: EntitySports.Stub,
+  base_url: "https://rest.entitysport.com/sandbox/cricket/",
+  token: "c16eaec3abd49e3477yy567836a95ad43"
